41c4f3b1c3ad7712831b3b8ba9f4e729152b22e7
CristiN <avinashukla2000@gmail.com>
2019-05-11 04:24:14 +0530

    Add Get/todos/:id

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -7,36 +7,6 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, client) => {
   console.log("Connected to MongoDB server");
   const db = client.db("TodoApp");
 
-  // db.collection("Todos").insertOne({
-  //   text: "Something to do",
-  //   completed: "false"
-  // }, (err, result) => {
-  //   if (err) {
-  //     return console.log("Unable to insert todo", err);
-  //   }
-  //   console.log(JSON.stringify(result.ops, undefined, 2));
-  // });
-  //   db.collection('User').insertOne({
-  //     name: "Avinash Shukla",
-  //     age: 19,
-  //     location: "Durg"
-  //   }, (err, result) => {
-  //     if (err) {
-  //       return console.log("Unable to insert todo", err);
-  //     }
-  //     console.log(JSON.stringify(result.ops, undefined, 2));
-  //   });
-  //   client.close();
-
-
-  // db.collection('Todos').find({
-  //   _id: '5cbdb30853e6201f92064123'
-  // }).toArray().then((docs) => {
-  //   console.log('Todos');
-  //   console.log(JSON.stringify(docs, undefined, 2));
-  // }, (err) => {
-  //   console.log("Unable to Fetch todos", err);
-  // });
 
 
   db.collection('Todos').find().count().then((count) => {
--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const {
+  ObjectID
+} = require('mongodb');
 
 var app = express();
 
@@ -14,6 +17,34 @@ var {
 } = require("./models/user");
 
 app.use(bodyParser.json());
+
+app.get("/todos", (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({
+      todos
+    })
+  }, (e) => {
+    res.status(400).send(e)
+  });
+});
+
+app.get("/todos/:id", (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({
+      todo
+    });
+  }, (e) => {
+    res.status(400).send();
+  })
+});
 
 app.post("/todos", (req, res) => {
   var todo = new Todo({
@@ -32,3 +63,7 @@ app.post("/todos", (req, res) => {
 app.listen(3000, () => {
   console.log("Server running on port 3000 🔥");
 });
+
+module.exports = {
+  app
+};
